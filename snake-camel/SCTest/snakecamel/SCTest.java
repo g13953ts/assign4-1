926391e9a11d26a7b46ca798c08d0571e4ce7d3b
g13953ts <g13953ts@s30456.tsuda.ac.jp>
2016-05-06 11:46:14 +0900

    テストケース修正

--- snake-camel/SCTest/snakecamel/SCTest.java
+++ snake-camel/SCTest/snakecamel/SCTest.java
@@ -1,14 +1,41 @@
 package snakecamel;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
 
 public class SCTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void stocTest() {
+		SnakeCamelUtil scUtil = new SnakeCamelUtil();
+		String expected = "AbCd";
+		String actual = scUtil.snakeToCamelcase("ab_cd");
+		assertThat(actual, is(not(expected)));
 	}
 
+	@Test
+	public void ctosTest() {
+		SnakeCamelUtil scUtil = new SnakeCamelUtil();
+		String expected = "abc_de";
+		String actual = scUtil.camelToSnakecase("AbcDe");
+		assertThat(actual, is(not(expected)));
+	}
+	
+	@Test
+	public void capTest() {
+		SnakeCamelUtil scUtil = new SnakeCamelUtil();
+		String expected = "Ab";
+		String actual = scUtil.capitalize("ab");
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void uncapTest() {
+		SnakeCamelUtil scUtil = new SnakeCamelUtil();
+		String expected = "aBC";
+		String actual = scUtil.uncapitalize("ABC");
+		assertThat(actual, is(expected));
+	}
 }
