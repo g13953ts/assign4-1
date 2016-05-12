200f4f0979ecf8527bf09c43fecf4dacba06536e
sakura <sakura@10.10.5.48>
2016-05-12 19:48:13 +0900

    課題4-4修正

---
+++ snake-camel/src/snakecamel/Main.java
@@ -0,0 +1,8 @@
+package snakecamel;
+
+public class Main {
+public static void main(String[] args) {
+	SnakeCamelUtil scUtil = new SnakeCamelUtil();
+	System.out.println(scUtil.snakeToCamelcase("abc"));
+}
+}
